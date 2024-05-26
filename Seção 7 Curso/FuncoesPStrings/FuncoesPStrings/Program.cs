bec894f02adc8937bc6de9fd24a08f186ca3bf9f
washington <washingtoncj2005@gmail.com>
2024-05-26 01:07:08 -0300

    Last Atualizations

--- Seção 7 Curso/FuncoesPStrings/FuncoesPStrings/Program.cs
+++ Seção 7 Curso/FuncoesPStrings/FuncoesPStrings/Program.cs
@@ -8,29 +8,44 @@ namespace FuncoesPStrings
             string original = " abc DEF GHI abc DEF ";
 
             int tam = original.Length; // Ver o tamanho do String todo e (contando os espaços)
-            int tam2 = original.Trim().Replace(" ", " ").Length; //substituir espaço vazio por nada
+            int tam2 = original.Trim().Replace(" ", " ").Length; //REPLACE substituir espaço vazio por nada
+            int p1 = original.IndexOf("bc"); // procura o bc dentro da string
+            int p2 = original.LastIndexOf("bc"); // procura os ultimos bc dentro da string
             string s1 = original.ToUpper(); // Grande
             string s2 = original.ToLower(); // Pequeno
             string s3 = original.Trim(); // Remove espaços em branco
-            int p1 = original.IndexOf("bc"); // procura o bc dentro da string
-            int p2 = original.LastIndexOf("bc"); // procura os ultimos bc dentro da string
             string s4 = original.Substring(3); // Recortar - Ver o caractere desta posição adiante
-            string s5 = original.Substring(3, 5); // Recortar - de TAL posição até TAL
+            string s5 = original.Substring(3, 5); // Recortar - de TAL a quantia depois do Especificado
+            string s6 = original.Replace('a','x'); // Susbstitui todos A por X no case CHAR
+            string s7 = original.Replace("abc","OLÁ"); // Susbstitu string por string
 
+            bool s8 = string.IsNullOrEmpty(original); // Testar se é vazio
+            bool s9 = string.IsNullOrWhiteSpace(original); // Testa se é um monte de espaço em branco
 
+            Console.WriteLine("original: " + original);
             Console.WriteLine("Tamanho do String" + tam);
             Console.WriteLine("Tamanho sem espaços" + tam2);
-            Console.WriteLine("original: " + original);
+            Console.WriteLine("IndexOf('bc'): " + p1);
+            Console.WriteLine("LastIndexOf('bc'): " + p2);
             Console.WriteLine("ToUpper: " + s1);
             Console.WriteLine("ToLower: " + s2);
             Console.WriteLine("Trim:" + s3);
-            Console.WriteLine("IndexOf('bc'): " + p1);
-            Console.WriteLine("LastIndexOf('bc'): " + p2);
             Console.WriteLine("substring(3)" + s4);
             Console.WriteLine("substring(3, 5)" + s5);
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
+            Console.WriteLine("Replace('a','x'): " + s6);
+            Console.WriteLine("Replace(Abc,OLÁ): " + s7);
+
+            Console.WriteLine("IsNullOrEmpty "+ s8);
+            Console.WriteLine("IsNullOrWhiteSpace " + s9);
+            Console.WriteLine();
+            if (string.IsNullOrEmpty(original) == false )
+            {
+                Console.WriteLine("Formulário não deve ser Vazio");
+            }
+            else
+            {
+                Console.WriteLine("Tudo Certo");
+            }
         }
     }
 }
