116cc8788b0d1915cefdf782c0c133126e50198c
washington <washingtoncj2005@gmail.com>
2024-05-28 22:12:37 -0300

    Seção 7 Completa

---
+++ Seção 7 Curso/Datetimekind/Datetimekind/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Datetimekind
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+
+            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Unspecified);
+
+            Console.WriteLine(d1);
+            Console.WriteLine();
+            Console.WriteLine(d1.Kind);
+            Console.WriteLine(d1.ToLocalTime()); // GMT local no caso GMT -3
+            Console.WriteLine(d1.ToUniversalTime()); // GMT
+            Console.WriteLine();
+
+            Console.WriteLine(d2.Kind);
+            Console.WriteLine(d2.ToLocalTime()); // GMT local no caso GMT -3
+            Console.WriteLine(d2.ToUniversalTime()); // GMT
+            Console.WriteLine();
+
+            Console.WriteLine(d3.Kind); // neste caso é Não especificado, ele subtrai e soma quando chamado
+            Console.WriteLine(d3.ToLocalTime()); // GMT local no caso GMT -3
+            Console.WriteLine(d3.ToUniversalTime()); // GMT
+            Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ Seção 7 Curso/PadraoISO/PadraoISO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace PadraoISO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime d2 = DateTime.Parse("2000-08-15T13:05:58Z");// cria o local DateTime
+
+            Console.WriteLine("d1: " + d1);
+            Console.WriteLine("d1 Kind: " + d1.Kind);
+            Console.WriteLine("d1 to Local: " + d1.ToLocalTime());
+            Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime());
+            Console.WriteLine();
+            Console.WriteLine("d2: " + d2);
+            Console.WriteLine("d2 Kind: " + d2.Kind);
+            Console.WriteLine("d2 to Local: " + d2.ToLocalTime());
+            Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime());
+            Console.WriteLine();
+
+            Console.WriteLine(d2.ToString("yyyy-MM-ddTHH:mm:ssZ"));
+            Console.WriteLine(d2.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"));
+            // Podemos ter problema aqui, PRECISAMOS garantir que Date esteja em formato Universal
+            // para fazermos o ToString e Formatar em ISO8601 - Como vemos na linha 23
+        }
+    }
+}
